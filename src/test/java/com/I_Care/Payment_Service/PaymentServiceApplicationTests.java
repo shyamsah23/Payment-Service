7182899023c37952b56a3aa062b26fcb63e911a5
shyamsah23 <92447257+shyamsah23@users.noreply.github.com>
2025-12-23 09:58:57 +0530

    Payment Service

---
+++ src/test/java/com/I_Care/Payment_Service/PaymentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.I_Care.Payment_Service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
